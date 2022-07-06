e978d2d6090bca1dcb8dc90ca5c140506daabf98
Tim <tymofii.dolenko@axelspringer.com>
2022-07-06 12:19:33 +0200

    Initial Commit

---
+++ AnyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnyApp
+//
+//  Created by tdolenko on 06.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
